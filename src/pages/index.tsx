e689850d8de985cc7999db0c1ec7aa7d966cb040
Rafael Pardim <rafaelmedeirospardim@gmail.com>
2022-08-15 11:09:16 -0300

    :construction: uppdating Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from '../../node_modules/next/head'
 
+import styles from './home.module.scss'
+
 export default function Home() {
   return (
     <>
@@ -7,7 +9,17 @@ export default function Home() {
         <title>ig.news | Home</title>
       </Head>
 
-      <h1>Hello World</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications<br/>
+            <span>for $9.98 month</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="Gril coding" />
+      </main>
     </>
   )
 }
