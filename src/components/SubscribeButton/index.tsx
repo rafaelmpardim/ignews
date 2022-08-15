fcb4a32378f6a22bd6eff841274bb77d91da055c
Rafael Pardim <rafaelmedeirospardim@gmail.com>
2022-08-15 11:34:01 -0300

    :construction: creating SubscribeButton component

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,12 @@
+import styles from './styles.module.scss'
+
+export function SubscribeButton() {
+  return (
+    <button
+      className={styles.subscribeButton}
+      type="button"
+    >
+      Subscribe now
+    </button>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from '../../node_modules/next/head'
+import { SubscribeButton } from '../components/SubscribeButton/index'
 
 import styles from './home.module.scss'
 
@@ -17,6 +18,7 @@ export default function Home() {
             Get access to all the publications<br/>
             <span>for $9.98 month</span>
           </p>
+          <SubscribeButton />
         </section>
         <img src="/images/avatar.svg" alt="Gril coding" />
       </main>
