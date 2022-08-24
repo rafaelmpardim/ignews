05bf927e005e90f5c6cf43dd36ebcfb28c0bad8d
Rafael Pardim <rafaelmedeirospardim@gmail.com>
2022-08-24 17:36:41 -0300

    :sparkles: integrating Prismic API

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,8 +1,25 @@
 import styles from './styles.module.scss'
 
 import Head from '../../../node_modules/next/head'
+import { GetStaticProps } from '../../../node_modules/next/types/index'
 
-export default function Posts() {
+import { getPrismicClient } from '../../services/prismic'
+import * as Prismic from "../../../node_modules/@prismicio/client/dist/index"
+import { RichText } from 'prismic-dom'
+
+type Post = {
+  slug: string,
+  title: string,
+  excerpt: string,
+  updatedAt: string
+}
+
+interface PostsProps {
+  posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps) {
+  
   return (
     <>
       <Head>
@@ -11,28 +28,51 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="">
-            <time>20/08/2022</time>
-            <strong>Lorem ipsum dolor sit amet, consectet</strong>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, numquam? Quas minus natus illo quis at. Impedit vel repellendus pariatur asperiores assumenda corporis sunt. Earum quasi sequi nobis? Odio, distinctio?</p>
-          </a>
-          <a href="">
-            <time>20/08/2022</time>
-            <strong>Lorem ipsum dolor sit amet, consectet</strong>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, numquam? Quas minus natus illo quis at. Impedit vel repellendus pariatur asperiores assumenda corporis sunt. Earum quasi sequi nobis? Odio, distinctio?</p>
-          </a>
-          <a href="">
-            <time>20/08/2022</time>
-            <strong>Lorem ipsum dolor sit amet, consectet</strong>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, numquam? Quas minus natus illo quis at. Impedit vel repellendus pariatur asperiores assumenda corporis sunt. Earum quasi sequi nobis? Odio, distinctio?</p>
-          </a>
-          <a href="">
-            <time>20/08/2022</time>
-            <strong>Lorem ipsum dolor sit amet, consectet</strong>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, numquam? Quas minus natus illo quis at. Impedit vel repellendus pariatur asperiores assumenda corporis sunt. Earum quasi sequi nobis? Odio, distinctio?</p>
-          </a>
+          {
+            posts.map(post => (
+              <a key={post.slug} href="#">
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            ))
+          }
         </div>
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient()
+  
+  const response = await prismic.query([
+    Prismic.predicates.at('document.type', 'p')
+  ],
+    {
+      fetch: ['p.title', 'p.content'],
+      pageSize: 100,
+    }
+  )
+
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.test ??  '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    }
+  })
+
+  console.log(JSON.stringify(response, null, 2))
+
+  return {
+    props: {
+      posts
+    }
+  }
 }
---
+++ src/services/prismic.ts
@@ -0,0 +1,12 @@
+import * as Prismic from "../../node_modules/@prismicio/client/dist/index"
+
+export function getPrismicClient() {
+  const prismic = Prismic.createClient(
+    process.env.PRISMIC_ENDPOINT,
+    {
+      accessToken: process.env.PRISMIC_ACCESS_TOKEN
+    }
+  )
+
+  return prismic
+}
