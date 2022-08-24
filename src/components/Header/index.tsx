5aaf3cc3d2b6859eaf6237f193ab05ff76c03219
Rafael Pardim <rafaelmedeirospardim@gmail.com>
2022-08-24 18:09:37 -0300

    :sparkles: implementing ActiveLink

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import { SignInButton } from '../SignInButton/index'
 
 import styles from './styles.module.scss'
+import { ActiveLink } from '../ActiveLink/index'
 
 export function Header() {
   return (
@@ -8,8 +9,12 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active} href="">Home</a>
-          <a href="">Posts</a>
+          <ActiveLink activeClassName={styles.active} href='/'>
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href='/posts'>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
